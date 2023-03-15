5429c6e2c9406173f2a6e2c0b4746d94f9edb847
Janet Webster <twixmixy@gmail.com>
2023-03-15 10:39:04 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  // var key = e.key;
+  if (e.key === "1") {
+    box1.innerText = "🤖";
+  } else if (e.key === "2") {
+    box2.innerText = "👽";
+  } else if (e.key === "3") {
+    box3.innerText = "🍄";
+  }
+});
+
+var clear = document.querySelector("button");
+
+clear.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
